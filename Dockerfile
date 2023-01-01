d1302191112f42c3c6825f9579068e23f473c3e0
Zverinus <75039044+Zverinus@users.noreply.github.com>
2023-01-01 19:08:19 +0300

    version 1.2

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
+ARG HUB_PATH
+ARG NOTEBOOKS_FROM
+
 FROM jupyterhub/jupyterhub:3.0.0
 
 RUN python3 -m pip install --no-cache-dir dockerspawner==12.1
 COPY ./jupyterhub_config.py /srv/jupyterhub/
+COPY $NOTEBOOKS_FROM $HUB_PATH
 EXPOSE 8000:8000
 CMD ["jupyterhub", "-f", "/srv/jupyterhub/jupyterhub_config.py"]
--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -5,11 +5,9 @@ c = get_config()
 c.JupyterHub.spawner_class = "dockerspawner.DockerSpawner"
 c.DockerSpawner.image = "jupyter/minimal-notebook:latest"
 
-c.DockerSpawner.use_internal_ip = True
 c.DockerSpawner.network_name = "jupyterhub-network"
 c.DockerSpawner.notebook_dir = "/home"
 c.DockerSpawner.volumes = {"jupyterhub-user-admin": "/home"}
-c.DockerSpawner.remove = True
 
 
 c.JupyterHub.hub_ip = "jupyterhub"
